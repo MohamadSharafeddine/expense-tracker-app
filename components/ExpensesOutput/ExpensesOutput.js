b5e0e414d09c8f9f692300f7c9fabb94bebd20a6
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-12-25 21:00:07 +0200

    Added dummy expenses data

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,43 @@ import React from "react";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    description: "A new shirt",
+    amount: 45.99,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    description: "A new phone",
+    amount: 799.49,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    description: "A new laptop",
+    amount: 1500,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: "e5",
+    description: "Groceries",
+    amount: 200,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList />
     </View>
   );
