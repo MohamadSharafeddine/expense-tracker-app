8a1cafe4c7146147ad7272400d1036fc82e16f4f
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-12-27 02:16:43 +0200

    Managed form and submission state

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import Input from "./Input";
+import Button from "../UI/Button";
+import { GlobalStyles } from "../../constants/styles";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({ submitButtonLabel, onCancel, onSubmit }) => {
   const [inputValues, setInputValues] = useState({
     amount: "",
     date: "",
@@ -17,6 +19,9 @@ const ExpenseForm = () => {
       };
     });
   };
+
+  const submitHandler = () => {};
+
   return (
     <View style={styles.form}>
       <Text style={styles.title}>Your Expense</Text>
@@ -49,6 +54,14 @@ const ExpenseForm = () => {
           value: inputValues.description,
         }}
       />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
     </View>
   );
 };
@@ -74,5 +87,15 @@ const styles = StyleSheet.create({
   },
   rowInput: {
     flex: 1,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });
